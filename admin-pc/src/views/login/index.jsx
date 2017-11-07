2919f8d9036fea2a5d659e680211a2221923030a
YXC000 <1352364202@qq.com>
2017-11-07 11:37:14 +0800

    1107

--- admin-pc/src/views/login/index.jsx
+++ admin-pc/src/views/login/index.jsx
@@ -25,15 +25,15 @@ class Login extends React.Component {
     render() {
         return (
             <div className="login">
-                <div className="loginInput">
+                <div className="login-input-box">
                     <div className="login-top">
-                        <span style={{ color: '#000', fontSize: 32 }}>友通环保后台管理系统</span>
+                        {/* <span style={{ color: '#000', fontSize: 32 }}>友通环保后台管理系统</span>
                         <div style={{ fontSize: 12, color: '#000', marginTop: 8, wordSpacing: 5 }}>
                             You Tong Huan Bao Hou Tai Guan Li Xi Tong
-                        </div>
+                        </div> */}
                     </div>
                     <div className="login-main">
-                        <div>用户登录:</div>
+                        <div className="login-title">用户登录</div>
                         <LoginForm></LoginForm>
                     </div>
                 </div>
@@ -109,17 +109,16 @@ class LoginForm extends React.Component {
         return (
             <div className="login-form">
                 <Input placeholder="请输入手机号码" value={this.state.phoneNumber}
-                    addonAfter={<Icon type="user"
-                        style={{ fontSize: 20 }} />}
+                    prefix={<Icon type="user" style={{ fontSize: 20, color: '#999' }} />}
                     onChange={this.changePhoneNum.bind(this)} />
                 <Input type="password" placeholder="请输入密码"
-                    addonAfter={<Icon type="lock"
-                        style={{ fontSize: 20, backgroundCor: '#000', }} />}
+                    prefix={<Icon type="lock" style={{ fontSize: 20, color: '#999' }} />}
                     onChange={this.changePsd.bind(this)} />
                 <div className="remember">
                     <Checkbox
                         onChange={this.remember.bind(this)}
-                        checked={this.state.remember}>记住用户名</Checkbox>
+                        checked={this.state.remember}>记住我的登录信息
+                    </Checkbox>
                 </div>
                 <Button type="primary" htmlType="submit"
                     onClick={this.handleSubmit.bind(this)} className="login-form-button">
